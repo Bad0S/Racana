38cef109f42ea8e8212db1c053495f1e43e18d1c
Unknown <awned@hotmail.fr>
2018-05-21 21:27:07 +0200

    Passe Arrière sur le droit chemin

--- Assets/scripts/Graphique/PasseArriere.cs
+++ Assets/scripts/Graphique/PasseArriere.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PasseArriere : MonoBehaviour 
 {
-	public List<GameObject> elementsPasseArriere;
+	public List<Transform> elementsPasseArriere;
 	private Transform childrenTrans;
 	private SpriteRenderer elementRend;
 	// Use this for initialization
@@ -15,18 +15,39 @@ public class PasseArriere : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		foreach (GameObject checkPasseArriere in elementsPasseArriere)
+		foreach (Transform checkPasseArriere in elementsPasseArriere)
 		{
-			childrenTrans = checkPasseArriere.GetComponentInChildren<Transform> ();
-			elementRend = checkPasseArriere.GetComponent<SpriteRenderer> ();
+            elementRend = checkPasseArriere.GetComponent<SpriteRenderer>();
+            childrenTrans = getFirstChildren(checkPasseArriere);
+            print(childrenTrans);
 			if (childrenTrans.position.y > transform.position.y) 
 			{
 				elementRend.sortingOrder = 1;
-			} 
+			}
 			if (childrenTrans.position.y < transform.position.y) 
 			{
 				elementRend.sortingOrder = 3;
 			}
 		}
 	}
+    private Transform getFirstChildren(Transform parent)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>();
+        Transform[] firstChildren = new Transform[parent.childCount];
+        int index = 0;
+        foreach (Transform child in children)
+        {
+            if (child.parent == parent)
+            {
+                firstChildren[index] = child;
+                index++;
+            }
+        }
+        if (firstChildren.Length == 0)
+        {
+            firstChildren = new Transform[1];
+            firstChildren[0] = GetComponentInChildren<Transform>();
+        }
+        return firstChildren[0];
+    }
 }
