0b1f149ecfaaa9e7217f4c3efecfee76a33a23a5
Unknown <awned@hotmail.fr>
2018-06-06 21:17:08 +0200

    Il n'est plus 20h30

--- Assets/scripts/Boss/BossScript.cs
+++ Assets/scripts/Boss/BossScript.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
+using UnityEngine.UI;
 
 
-public class BossScript : MonoBehaviour {
-
+public class BossScript : MonoBehaviour 
+{
+	public Image BossBar;
+	public Image BossBarFill;
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+			
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		BossBarFill.fillAmount = GetComponent <health> ().life / 72;
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
