417a84de1d02976d95afdcf7a0db08ac126d2d6d
Unknown <leo.boudoin@gmail.com>
2018-06-10 23:51:02 +0200

    ref gd prop

---
+++ Assets/scripts/prez/StartAnim.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnim : MonoBehaviour {
+	public GameObject animated;
+
+	public void StartAnimation(){
+		animated.GetComponent <Animator>().SetBool ("animate", true);//1391
+	}
+}
